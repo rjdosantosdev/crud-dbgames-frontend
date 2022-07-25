303ff5b0097c6315dcab535d58dbd7da2ff5afcb
romariojsx <romariojdosantos@gmail.com>
2022-07-25 13:57:28 -0300

    api

--- src/components/GamesList/GamesList.jsx
+++ src/components/GamesList/GamesList.jsx
@@ -1,5 +1,7 @@
+import { Box, Card, Grid, Container } from "@mui/material";
 import React from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
 
 function GamesList() {
   const [dados, setDados] = React.useState(null);
@@ -12,18 +14,32 @@ function GamesList() {
       .catch((error) => console.log(error));
   }, []);
   return (
-    <div>
-      <ul>
-        {dados &&
-          dados.map((item) => (
-            <li key={item.id}>
-              <p>{item.game}</p>
-              <p>R$ {item.cost}</p>
-              <p>{item.category}</p>
-            </li>
-          ))}
-      </ul>
-    </div>
+    <Box sx={{ marginTop: "60px" }}>
+      <Container maxWidth="sm">
+        <Card sx={{ padding: "20px" }}>
+          <ul>
+            {dados &&
+              dados.map((item) => (
+                <Grid
+                  sx={{
+                    border: "1px solid #e7e7e7",
+                    padding: "10px 20px",
+                    marginTop: "-1px",
+                  }}
+                >
+                  <li key={item.id}>
+                    <Link to="/single-product">
+                      <h2 sx={{ marginBottom: "8px" }}>{item.game}</h2>
+                    </Link>
+                    <p>R$ {item.cost}</p>
+                    <p>{item.category}</p>
+                  </li>
+                </Grid>
+              ))}
+          </ul>
+        </Card>
+      </Container>
+    </Box>
   );
 }
 
