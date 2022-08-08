5c6b3f245b725b9db2bcf2d98a1ca1b52736241e
romariojsx <romariojdosantos@gmail.com>
2022-08-08 15:54:46 -0300

    CRUD (CREATE)

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,85 +1,130 @@
 import React from "react";
 import api from "../../services/api";
 import { Box, Button, TextField, Typography } from "@mui/material";
-import { Formik } from "formik";
 
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import schema from "./schema";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Link
+import { Link } from "react-router-dom";
 function HomePage() {
-  const [inputGame, setInputGame] = React.useState("");
-  const [inputCost, setInputCost] = React.useState("");
-  const [inputCategory, setInputCategory] = React.useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    const inputs = {
-      game: inputGame,
-      cost: inputCost,
-      category: inputCategory,
-    };
-    api.post("/send", inputs).then((response) => {
-      console.log(response);
+  function handleSubmit(values, actions) {
+    // const inputs = {
+    //   game: inputGame,
+    //   cost: inputCost,
+    //   category: inputCategory,
+    // };
+    api.post("/send", values).then((response) => {
       if (response.data === true) {
-        alert("cadastro feito com sucesss");
+        toast.success("Cadastro realizado com sucesso!", {
+          position: toast.POSITION.TOP_RIGHT,
+        });
       } else {
-        console.log("erro no cadastro");
+        toast.error("Erro ao preencher os campos!", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
       }
     });
   }
+
   return (
-    <form>
-      <Typography
-        sx={{
-          marginBottom: 4,
-          marginTop: 8,
+    <>
+      <Formik
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+        initialValues={{
+          game: "",
+          cost: "",
+          category: "",
         }}
-        variant="h4"
-        component="h2"
-        textAlign="center"
       >
-        Cadastrar novo Jogo
-      </Typography>
-      <Box container width="600px" margin="0 auto" height="65vh">
-        <TextField
-          margin="normal"
-          fullWidth
-          id="game"
-          name="game"
-          label="game"
-          type="text"
-          onChange={(e) => setInputGame(e.target.value)}
-        />
-        <TextField
-          required
-          margin="normal"
-          fullWidth
-          id="cost"
-          name="cost"
-          label="cost"
-          type="number"
-          onChange={(e) => setInputCost(e.target.value)}
-        />
-        <TextField
-          fullWidth
-          margin="normal"
-          id="category"
-          name="category"
-          label="category"
-          type="text"
-          onChange={(e) => setInputCategory(e.target.value)}
-        />
-        <Button
-          onClick={handleSubmit}
-          margin="normal"
-          type="submit"
-          fullWidth
-          variant="contained"
-          size="large"
-          color="primary"
-          sx={{ mt: 3, mb: 2, fontSize: 18 }}
-        >
-          cadastrar
-        </Button>
-      </Box>
-    </form>
+        {({ values, actions, errors }) => (
+          <Form>
+            <Typography
+              sx={{ marginBottom: 4, marginTop: 8 }}
+              variant="h4"
+              component="h2"
+              textAlign="center"
+            >
+              Criar novo jogo
+            </Typography>
+            <Box container maxWidth={400} margin="0 auto" height="65vh">
+              <Field
+                as={TextField}
+                fullWidth
+                name="game"
+                label="Nome do Jogo"
+                type="text"
+              />
+
+              <Box mt={0.75}>
+                <Typography
+                  width={400}
+                  component="h6"
+                  variant="caption"
+                  color="error"
+                  fontWeight="regular"
+                >
+                  <ErrorMessage name="game" />
+                </Typography>
+              </Box>
+
+              <Field
+                fullWidth
+                as={TextField}
+                label="Preço"
+                name="cost"
+                type="number"
+              />
+              <Box mt={0.75}>
+                <Typography
+                  width={400}
+                  component="h6"
+                  variant="caption"
+                  color="error"
+                  fontWeight="regular"
+                >
+                  <ErrorMessage name="cost" />
+                </Typography>
+              </Box>
+              <Field
+                as={TextField}
+                fullWidth
+                label="Categoria"
+                name="category"
+                type="text"
+              />
+              <Box mt={0.75}>
+                <Typography
+                  width={400}
+                  component="h6"
+                  variant="caption"
+                  color="error"
+                  fontWeight="regular"
+                >
+                  <ErrorMessage name="category" />
+                </Typography>
+              </Box>
+              <Button
+                fullWidth
+                margin="normal"
+                type="submit"
+                size="large"
+                variant="contained"
+                sx={{ mt: 3, mb: 2, fontSize: 18 }}
+              >
+                Cadastrar
+              </Button>
+              <Typography align="center">
+                <Link to="/games">Lista de Jogos</Link>
+              </Typography>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 }
 
