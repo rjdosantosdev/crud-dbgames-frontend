6b2477ed3fd412c13f13edf56800fc5408a33d6b
romariojsx <romariojdosantos@gmail.com>
2022-08-16 13:40:52 -0300

    heroku rota

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-  // baseURL: "https://cruddbgames.herokuapp.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://cruddbgames.herokuapp.com",
+  // baseURL: "http://localhost:8080",
   // headers: {
   //   "Content-type": "application/json",
   // },
